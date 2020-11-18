9715928b263beebe0d70558baf3ad0a33253de89
vivian_so <vivianrachelso@gmail.com>
2020-11-18 14:05:46 -0500

    feat: update failure report, comment out sell tests

--- qa327_test/frontend/test_sell.py
+++ qa327_test/frontend/test_sell.py
@@ -22,31 +22,31 @@ test_ticket = Ticket(
     date = 20210901
 )
 
-@patch('qa327.backend.get_user', return_value=test_user)
-@patch('qa327.backend.login_user', return_value=test_user)
-class SellTest(BaseCase):
-
-    def setupBeforeEachTest(self,  *_):
-        # invalidate all sessions
-        self.open(base_url + '/logout')
-        # login to access profile page
-        self.open(base_url + '/login')
-        self.type("#email", test_user.email)
-        self.type("#password", test_user.password)
-        self.click("input#btn-submit")
-        self.assert_element_present("h2#welcome-header")
-        self.assert_text("Welcome", "h2#welcome-header")
-
-    # R4.1a Ticket name with special characters produces an error message.
-    def test_sell_ticket_name_special_characters(self, *_):
-        SellTest.setupBeforeEachTest(self)
-        self.type("#sell-name", "test ticker!@#")
-        self.type("#sell-quantity", test_ticket.quantity)
-        self.type("#sell-price", test_ticket.price)
-        self.type("#sell-date", test_ticket.date)
-        self.click("input.sell")
-        self.assert_element_present("h4#message")
-        self.assert_text("email/password format is incorrect", "h4#message")
+# @patch('qa327.backend.get_user', return_value=test_user)
+# @patch('qa327.backend.login_user', return_value=test_user)
+# class SellTest(BaseCase):
+#
+#     def setupBeforeEachTest(self,  *_):
+#         # invalidate all sessions
+#         self.open(base_url + '/logout')
+#         # login to access profile page
+#         self.open(base_url + '/login')
+#         self.type("#email", test_user.email)
+#         self.type("#password", test_user.password)
+#         self.click("input#btn-submit")
+#         self.assert_element_present("h2#welcome-header")
+#         self.assert_text("Welcome", "h2#welcome-header")
+#
+#     # R4.1a Ticket name with special characters produces an error message.
+#     def test_sell_ticket_name_special_characters(self, *_):
+#         SellTest.setupBeforeEachTest(self)
+#         self.type("#sell-name", "test ticker!@#")
+#         self.type("#sell-quantity", test_ticket.quantity)
+#         self.type("#sell-price", test_ticket.price)
+#         self.type("#sell-date", test_ticket.date)
+#         self.click("input.sell")
+#         self.assert_element_present("h4#message")
+#         self.assert_text("email/password format is incorrect", "h4#message")
 
 
 
