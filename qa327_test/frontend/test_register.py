05e5074fe5f7c8eb51a026cc1769ae4eddf97c91
Adrian <16ar12@queensu.ca>
2020-12-03 16:34:17 -0500

    Removed sample info comment from test_register

--- qa327_test/frontend/test_register.py
+++ qa327_test/frontend/test_register.py
@@ -6,21 +6,6 @@ from unittest.mock import patch
 from qa327.models import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
 
-"""
-This file defines all unit tests for the frontend homepage.
-
-The tests will only test the frontend portion of the program, by patching the backend to return
-specfic values. For example:
-
-@patch('qa327.backend.get_user', return_value=test_user)
-
-Will patch the backend get_user function (within the scope of the current test case)
-so that it return 'test_user' instance below rather than reading
-the user from the database.
-
-Annotate @patch before unit tests can mock backend methods (for that testing function)
-"""
-
 # Mock a sample user
 test_user = User(
     email='test_frontend@test.com',
